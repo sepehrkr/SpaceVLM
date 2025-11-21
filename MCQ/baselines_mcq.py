4469f9fc37c55f3568c75bcc1579f8b005ff3f8b
Sepehr Kazemi <83524104+sepehrkr@users.noreply.github.com>
2025-11-21 12:19:25 +0330

    Change default project_root argument path
    Signed-off-by: Sepehr Kazemi <83524104+sepehrkr@users.noreply.github.com>

--- MCQ/baselines_mcq.py
+++ MCQ/baselines_mcq.py
@@ -195,7 +195,7 @@ def video_retreival_evaluation(model, dataLoader, device) :
 def main():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("--project_root", type=str, default='/home/amirhossein.hajimohammadrezaie/ECOR_extended/Negation')
+    parser.add_argument("--project_root", type=str, default='./Negation')
     parser.add_argument("--dataset", type=str, default='coco', choices=['coco', 'voc2007', 'msr_vtt'])
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--pretrained_model", type=str, default='clip', choices=['clip', 'clip_negfull', 'con_clip', 'neg_clip', 'neg_clip_negfull'])
